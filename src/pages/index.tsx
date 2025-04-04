cc27c9ec27aea82ea4b6590f3230b3c62dc302b1
mayank <mayank@mayanks-MacBook-Air.local>
2025-04-05 02:49:50 +0530

    delete unuse block

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import NextUpBanner from '@/components/NextUpBanner';
 
 export default function Home() {
